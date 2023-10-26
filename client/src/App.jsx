e9ccbb77e87f7eb371bfb9408b2a9516c0f22066
santilagos <sntlgs@gmail.com>
2023-10-26 12:57:17 -0300

    edit analytics

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,9 @@ import Projects from "./sections/Projects";
 import Contact from "./sections/Contact";
 import Footer from "./components/Footer";
 import TechnologiesContainer from "./sections/TechnologiesContainer";
+import { inject } from '@vercel/analytics';
 
+inject();
 function App() {
   return (
     <div className="flex flex-col items-center text-mytext text-lg font-serif dark:text-mytextd">
